fc0e950475cf22e5c2ff9d55e8a4236c24b18005
Megan Horsfield <meganthadev@gmail.com>
2020-07-06 05:29:51 +0000

    Automatically backed up by Learn

--- db/migrate/20200706052541_create_preferences.rb
+++ db/migrate/20200706052541_create_preferences.rb
@@ -1,7 +1,7 @@
 class CreatePreferences < ActiveRecord::Migration[5.0]
   def change
     create_table :preferences do |t|
-      t.string :song_sort_order.string
+      t.string :song_sort_order
       t.string :artist_sort_order
       t.boolean :allow_create_songs
       t.boolean :allow_create_artists
